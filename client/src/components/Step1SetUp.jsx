b32f2d85791ca6aa59e4d3fcbd6fd4fe9cdaacfa
Aman Kumar <amandsvv4006@gmail.com>
2026-03-07 05:08:03 +0530

    Step1 completed, questions are being generated

--- client/src/components/Step1SetUp.jsx
+++ client/src/components/Step1SetUp.jsx
@@ -9,9 +9,12 @@ import {
     FaChartLine,
 } from "react-icons/fa";
 import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
 
 function Step1SetUp({ onStart }) {
-
+    const {userData} = useSelector((state) => state.user);
+    const dispatch = useDispatch();
     const [role, setRole] = useState("");
     const [experience, setExperience] = useState("");
     const [mode, setMode] = useState("Technical")
@@ -34,8 +37,6 @@ function Step1SetUp({ onStart }) {
         try {
             const result = await axios.post(serverUrl + "/api/interview/resume", formData, {withCredentials : true})
 
-            console.log(result.data)
-
             setRole(result.data.role || "");
             setExperience(result.data.experience || "");
             setProjects(result.data.projects || []);
@@ -51,6 +52,21 @@ function Step1SetUp({ onStart }) {
         }
     }
 
+    const handleStart = async() => {
+        setLoading(true);
+        try {
+            const result = await axios.post(serverUrl + "/api/interview/generate-questions",{role, experience, mode, resumeText, projects, skills}, {withCredentials : true});
+
+            if(userData){
+                dispatch(setUserData({...userData, credits : result.data.creditLeft}))
+            }
+            setLoading(false);
+            onStart(result.data)
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
+    }
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -189,11 +205,12 @@ function Step1SetUp({ onStart }) {
                             </motion.div>
                         )}
                         <motion.button 
-                        disabled={!role || !experience}
+                        onClick={handleStart}
+                        disabled={!role || !experience || loading}
                         whileHover={{scale : 1.09}}
                         whileTap={{scale: 0.95}}
                         className='w-full disabled:bg-gray-600 bg-green-600 hover:bg-green-600  text-white py-3 rounded-full text-lg font-semibold transition duration-300 shadow-md'>
-                            Start Interview
+                            {loading ? "Loading...." : "Start Interview"}
                         </motion.button>
                     </div>
                 </motion.div>
--- server/routes/interview.routes.js
+++ server/routes/interview.routes.js
@@ -1,10 +1,13 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
-import { analyzeResume } from "../controllers/interview.controllers.js"
+import { analyzeResume, finishInterview, generatequestions, submitAnswer } from "../controllers/interview.controllers.js"
 
 const interviewRouter = express.Router()
 
-interviewRouter.post("/resume", isAuth, upload.single("resume"), analyzeResume)
+interviewRouter.post("/resume", isAuth, upload.single("resume"), analyzeResume);
+interviewRouter.post("/generate-questions", isAuth, generatequestions);
+interviewRouter.post("/submit-ansswer", isAuth, submitAnswer);
+interviewRouter.post("/finish", isAuth, finishInterview);
 
 export default interviewRouter;
